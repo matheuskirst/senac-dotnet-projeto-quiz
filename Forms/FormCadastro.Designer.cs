655d1498739e3e4dc825b91b538bd7d1ecc7c5fa
matheus <matheusaugustokirst@gmail.com>
2026-08-11 21:52:15 -0300

    Adiciona Forms Cadastro e Login

--- Forms/FormCadastro.Designer.cs
+++ Forms/FormCadastro.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace SenacQuizApp
+﻿namespace SenacQuizApp.Forms
 {
-    partial class Form1
+    partial class FormCadastro
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,15 +23,22 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // FormCadastro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 433);
+            MinimumSize = new Size(640, 480);
+            Name = "FormCadastro";
+            Text = "Quiz App - Cadastro";
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ Forms/FormLogin.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SenacQuizApp.Forms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LoginMainLayout = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // LoginMainLayout
+            // 
+            LoginMainLayout.ColumnCount = 3;
+            LoginMainLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            LoginMainLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 90F));
+            LoginMainLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            LoginMainLayout.Dock = DockStyle.Fill;
+            LoginMainLayout.Location = new Point(0, 0);
+            LoginMainLayout.Name = "LoginMainLayout";
+            LoginMainLayout.RowCount = 4;
+            LoginMainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            LoginMainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 30F));
+            LoginMainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            LoginMainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            LoginMainLayout.Size = new Size(622, 433);
+            LoginMainLayout.TabIndex = 0;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(622, 433);
+            Controls.Add(LoginMainLayout);
+            MinimumSize = new Size(640, 480);
+            Name = "FormLogin";
+            Text = "Quiz App - Login";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel LoginMainLayout;
+    }
+}
