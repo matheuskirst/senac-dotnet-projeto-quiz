45f514b0324af7f4ed54fe26c9597a9536a5fa9a
NATHALYCATIELELOPESS <nathalysisnande@gmail.com>
2026-08-19 21:05:37 -0300

    adiconar modelos do Quiz

---
+++ Modelos/QuizPerguntas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenacQuizApp.Modelos
+{
+    public class QuizPerguntas
+    {
+        public int Id {  get; set; }
+
+        public int QuizId { get; set; }
+
+        public Quiz Quiz { get; set; } = null!;
+
+        public int PerguntaId { get; set; }
+
+        public Pergunta Pergunta { get; set; } = null!;
+
+
+
+    }
+}
